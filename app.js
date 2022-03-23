3fc6606aa0c6043c03189256fd48015f713a7f34
Kena737 <singhamitsboss@gmail.com>
2022-03-23 17:30:19 -0400

    h

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
-console.log("hey! We are in")
+console.log("hey! We are in");
 
-let addbtn = document.getElementById("addBtn")
-addbtn.addEventListener('click', function(e){
-    let addTxt = document.getElementById("addtxt")
-    let notes = localStorage.getItem('notes')
-    if(notes == null){
-            notesObj = []
-    }
-    else{
-        notesObj = JSON.parse(notes)
-    }
-})
+let addbtn = document.getElementById("addBtn");
+addbtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addtxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+  showNotes();
+});
 
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+      <div class="card-body">
+          <h5 class="card-title">Note ${index + 1}</h5>
+          <p class="card-text"> ${element}</p>
+          <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+      </div>
+  </div>`;
+  });
+  let notesELm = document.getElementById("notes");
+  if (notesObj.lenght != 0) {
+    notesELm.innerHTML = html;
+  } else {
+    notesELm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
+
+// Function to delete a note
+function deleteNote(index){
+    // console.log("I am deleting", index)
+    let notes = localStorage.getItem("notes")
+    if (notes == null){
+        notesObj = [];
+    }
+}
